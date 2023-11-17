05e7029632334c1928790e17b9d35db85ad44310
Sarah Qiao <bqia823@aucklanduni.ac.nz>
2023-11-17 19:18:31 +1300

    First commit

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+
+    const headerStyle = {
+        backgroundColor: 'mediumblue',
+        color: '#fff'
+    };
+
+    return (
+        <header style={headerStyle}>
+            <h1> Shopping list </h1>
+        </header>
+    );
+}
+
+export default Header;
